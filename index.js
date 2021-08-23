a402d7085cc78debd90dc2d9a2148db63b75971e
arctdav <32494274+arctdav@users.noreply.github.com>
2021-08-22 20:59:52 -0400

    changed

--- index.js
+++ index.js
@@ -32,17 +32,18 @@ runCommand('git', ['clone', repoURL, name])
   });
 
 function runCommand(command, args, options = undefined) {
-  const spawned = spawn(command, args, options);
+  const spawned = spawn(command, args, options)
+    .on('error', function (err) { throw err });
 
   return new Promise((resolve) => {
     spawned.stdout.on('data', (data) => {
       console.log(data.toString());
     });
-    
+
     spawned.stderr.on('data', (data) => {
       console.error(data.toString());
     });
-    
+
     spawned.on('close', () => {
       resolve();
     });
